cd85726e1b255ff1b017e7e0311727e7db65e09f
Ben Caccia <reverbtank@gmail.com>
2020-01-01 10:50:54 -0500

    refactor: byte counting code now in getTotalBytes

--- wc.go
+++ wc.go
@@ -7,6 +7,24 @@ import (
 	"os"
 )
 
+func getTotalBytes(scanner *bufio.Scanner) (total int) {
+
+	// define a variable to hold the bytes total
+	var bytesTotal = 0
+
+	// iterate through everything in the scanner and count the bytes
+	for scanner.Scan() {
+		// these two lines are for debugging only
+		//fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		//fmt.Println(int(len(scanner.Bytes())) + 1)
+
+		// convert the bytes on the line to an integer and add 1 to make it human readable
+		bytesTotal += int(len(scanner.Bytes()) + 1)
+	}
+	// print out the computed total bytes
+	return bytesTotal
+}
+
 func main() {
 	// define all the available flags for this command
 	byteFlag := flag.BoolP("bytes", "c", false, "print the byte counts")
@@ -29,32 +47,34 @@ func main() {
 	args := flag.Args()
 	fmt.Println(args)
 
+	// define a scanner to read from stdin
+	scanner := bufio.NewScanner(os.Stdin)
+
 	// check if any filename was passed as an arg. if so, take action
 	if len(args) < 1 {
 		fmt.Println("no args, let's parse from stdin")
-		// define a scanner to read from stdin
-		scanner := bufio.NewScanner(os.Stdin)
 
-		// define a variable to hold the bytes total
-		var bytesTotal = 0
+		total := getTotalBytes(scanner)
 
-		// iterate through everything in the scanner and count the bytes
-		for scanner.Scan() {
-			// these two lines are for debugging only
-			//fmt.Println(scanner.Text()) // Println will add back the final '\n'
-			//fmt.Println(int(len(scanner.Bytes())) + 1)
-
-			// convert the bytes on the line to an integer and add 1 to make it human readable
-			bytesTotal += int(len(scanner.Bytes()) + 1)
-		}
-		// print out the computed total bytes
-		fmt.Println(bytesTotal)
+		fmt.Println(total)
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+			// exit and indicate failure
+			os.Exit(1)
 		}
 	} else {
 		fmt.Println("args provided, let's parse out the filename")
 		fmt.Println(args)
-	}
+		for _, element := range args {
+			file, err := os.Open(element)
 
+			if err != nil {
+				fmt.Println("Failed to open the file: %s", element)
+				// exit and indicate failure
+				os.Exit(1)
+			}
+			defer file.Close()
+		}
+
+	}
 }
